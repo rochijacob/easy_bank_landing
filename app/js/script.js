addfc37cda7a2d1d9592b8fa48c64ebdda90c2d8
rochijacob <rochijacob@gmail.com>
2021-02-03 17:25:36 -0300

    third

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function(){
+
+    if(header.classList.contains('open')){          //Close menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    }
+    else {                              //Open Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+    
+});
